d04e7c243283111f74b7c8f94842642813235323
Trunks2k1ynwa <trungbts3@gmail.com>
2023-07-01 09:43:12 +0700

    Update branch routing

---
+++ app/dashboard/template.tsx
@@ -0,0 +1,6 @@
+export default function Template({ children }: { children: React.ReactNode }) {
+  return <div className="m-5 flex-1">
+    <h1 className="pb-5">Tempalte Dashboard</h1>
+    {children}
+  </div>
+}

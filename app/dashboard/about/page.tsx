6fc10eb6b4f43ef35c32313bd69387bd2ddc8cb3
Trunks2k1ynwa <trungbts3@gmail.com>
2023-07-07 18:45:54 +0700

    update error metaData

--- app/dashboard/about/page.tsx
+++ app/dashboard/about/page.tsx
@@ -1,4 +1,3 @@
-'use client';
 import Button from '@/components/atoms/Button';
 import React from 'react';
 
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,3 @@
-'use client';
 import Link from 'next/link';
 import './global.scss';
 import { Advent_Pro } from 'next/font/google';
